bd84b88fd6fe2c133c164e7005d742510337b9c9
Kayky Matos <kayky7277@gmail.com>
2023-04-21 15:23:06 -0300

    [5-IdentityServer] Criação do projeto IdentityServer para cuidar da parte de autenticação. DuendeSoftware sendo usando no projeto para facilitar a implementação

---
+++ GeekShoopping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeekShoopping.IdentityServer.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string FisrtName { get; set; }
+        private string LastName { get; set; }
+    }
+}
---
+++ GeekShoopping.IdentityServer/Model/Context/MySqlContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShoopping.IdentityServer.Model.Context
+{
+    public class MySqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySqlContext()
+        {
+        }
+        public MySqlContext(DbContextOptions<MySqlContext> oprions) : base(oprions) { }
+
+    }
+}
