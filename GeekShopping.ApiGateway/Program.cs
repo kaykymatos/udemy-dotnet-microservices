01c833256cbd68f2d1f4acf5da05f595c510f9a3
Kayky Matos <kayky7277@gmail.com>
2023-07-16 14:11:17 -0300

    [13-ocelot-service] Projeto ocelot criado.

---
+++ GeekShopping.ApiGateway/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseOcelot();
